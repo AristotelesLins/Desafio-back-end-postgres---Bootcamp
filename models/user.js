a0f038ba1ddcb6aa110f464c00e874d4f8ec8c42
Aristoteles Lins <aristoteles.lins.silva@gmail.com>
2025-03-15 00:07:59 -0300

    criação da migration user, e seu respectivo model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize) => {
+    const { DataTypes } = require('sequelize');
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        timestamps: true,
+        tablename: 'users'
+    });
+    return User;
+};
